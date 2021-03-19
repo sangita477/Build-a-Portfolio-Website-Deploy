95e6ca8fd363f329d80e7392a1d5c65082a3f731
Sangita Lama <80032427+sangita477@users.noreply.github.com>
2021-03-19 16:20:12 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log ('its working')
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener ('click', function(){
+		let mode = this.dataset.mode
+		console.log('option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementsById('theme-style').href = 'default.css'
+	}
+	if(mode == 'blue'){
+		document.getElementsById('theme-style').href = 'blue.css'
+	}
+	if(mode == 'green'){
+		document.getElementsById('theme-style').href = 'green.css'
+	}
+	if(mode == 'purple'){
+		document.getElementsById('theme-style').href = 'purple.css'
+	}
+	localStorage.setItem('theme', mode)
+}
